7e99694bd2ece80863f3647954f29212b57ae4b2
zspro <136277889@qq.com>
2022-03-23 11:16:02 +0800

    1

---
+++ jdPlantBeanShareCodes.js
@@ -0,0 +1,32 @@
+/*
+京东种豆得豆互助码
+此文件为Node.js专用。其他用户请忽略
+支持京东N个账号
+ */
+//云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
+// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
+// 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
+let PlantBeanShareCodes = [
+  //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
+  'wtyyzn25dg4xhnt62fxxtpjuweybgzsd5mwk6xa@2hgl5cknzhylc2nyromtm4fgbvwjtvpubucmayy@e7lhibzb3zek2air5q7463ng52gcjaw5dryclhq@mlrdw3aw26j3wevqw3htq7cmhrlletw3uf6fowi@4npkonnsy7xi34bodxhke4wjouyhl2cgkemjrwa@3xbil62tcqxjb6dsuyw4lnzhk4@4npkonnsy7xi2gferus7axvoc4k7kfwjsizej2i@4npkonnsy7xi3a3e572jwctwwhhoilkxydbudka@ky2g4ruv3mis35d4jp6cjz4gz4@ulo5spchi6yj4ufquptdxg6vey',
+  //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
+  'wtyyzn25dg4xhnt62fxxtpjuweybgzsd5mwk6xa@2hgl5cknzhylc2nyromtm4fgbvwjtvpubucmayy@e7lhibzb3zek2air5q7463ng52gcjaw5dryclhq@mlrdw3aw26j3wevqw3htq7cmhrlletw3uf6fowi@4npkonnsy7xi34bodxhke4wjouyhl2cgkemjrwa@3xbil62tcqxjb6dsuyw4lnzhk4@4npkonnsy7xi2gferus7axvoc4k7kfwjsizej2i@4npkonnsy7xi3a3e572jwctwwhhoilkxydbudka@ky2g4ruv3mis35d4jp6cjz4gz4@ulo5spchi6yj4ufquptdxg6vey',
+]
+// 判断github action里面是否有种豆得豆互助码
+if (process.env.PLANT_BEAN_SHARECODES) {
+  if (process.env.PLANT_BEAN_SHARECODES.indexOf('&') > -1) {
+    console.log(`您的种豆互助码选择的是用&隔开\n`)
+    PlantBeanShareCodes = process.env.PLANT_BEAN_SHARECODES.split('&');
+  } else if (process.env.PLANT_BEAN_SHARECODES.indexOf('\n') > -1) {
+    console.log(`您的种豆互助码选择的是用换行隔开\n`)
+    PlantBeanShareCodes = process.env.PLANT_BEAN_SHARECODES.split('\n');
+  } else {
+    PlantBeanShareCodes = process.env.PLANT_BEAN_SHARECODES.split();
+  }
+} else if (process.env.JD_COOKIE) {
+  console.log(`您secret里面未提供助力码，优先进行自己账号内互助，然后再给脚本内置的码进行助力，请知晓！`)
+}
+for (let i = 0; i < PlantBeanShareCodes.length; i++) {
+  const index = (i + 1 === 1) ? '' : (i + 1);
+  exports['PlantBeanShareCodes' + index] = PlantBeanShareCodes[i];
+}
