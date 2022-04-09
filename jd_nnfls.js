eabfb80f967b552ca4eae63e5b3931651903ef7e
zspro <136277889@qq.com>
2022-04-09 20:52:02 +0800

    1

--- jd_nnfls.js
+++ jd_nnfls.js
@@ -92,7 +92,7 @@ if ($.isNode()) {
 function getShareCode(name, num = -1, shuffle = false) {
     return new Promise(resolve => {
         $.get({
-            url: `https://raw.fastgit.org/zspro/updateTeam/main/shareCodes/${name}`,
+            url: `https://raw.githubusercontent.com/zspro/updateTeam/main/shareCodes/${name}`,
             headers: {
                 "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
             }
